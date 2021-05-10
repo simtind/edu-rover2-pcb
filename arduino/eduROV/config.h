4f8c82a138a7fe3a6b2f5b5453505c8d7d901f12
Simen August Tinderholt <simtind@users.noreply.github.com>
2021-05-10 22:39:26 +0200

    Fixes from testing.

--- arduino/eduROV/config.h
+++ arduino/eduROV/config.h
@@ -1,13 +1,16 @@
-#pragma once
+#ifndef _EDUROV_CONFIG_H_
+#define _EDUROV_CONFIG_H_
 
 // ----- Other I/O -----
 #define PIN_SENSE_BATTERY A5
 #define PIN_SENSE_PRESSURE A6
 #define PIN_SENSE_TEMPERATURE A7
 
-#define BATTERY_ADC_TO_VOLT(adc_value)     map(adc_value,  0, 1023, 0, 150)  // 0V to 5V in, multiply by 3 for divider.
-#define PRESSURE_ADC_TO_KPA(adc_value)     map(adc_value, 41, 1023, 0, 250) // 0.2V to 5V -> 0kPa to 250kPa
-#define TEMPERATURE_ADC_TO_DEGC(adc_value) map(adc_value,  0, 1023, 0, 50)  // 5V / (10mV / degC) = 50 degC  
+#define SENSOR_SCALE 100
+
+#define BATTERY_ADC_TO_VOLT(adc_value)     (int16_t)map(adc_value,  0, 1023, 0, 150 * SENSOR_SCALE)  // 0V to 5V in, multiply by 3 for divider.
+#define PRESSURE_ADC_TO_KPA(adc_value)     (int16_t)map(adc_value, 41, 1023, 0, 250 * SENSOR_SCALE) // 0.2V to 5V -> 0kPa to 250kPa
+#define TEMPERATURE_ADC_TO_DEGC(adc_value) (int16_t)map(adc_value,  0, 1023, 0, 50  * SENSOR_SCALE)  // 5V / (10mV / degC) = 50 degC  
 
 #define PIN_LED_PWM 3
 
@@ -17,10 +20,11 @@
 #define PIN_SENSE_MOTOR2 A2
 #define PIN_SENSE_MOTOR3 A1
 #define PIN_SENSE_MOTOR4 A0
-#define MOTOR_ADC_TO_AMP(adc_value) map(adc_value, 0, 1023, -5, 5) 
+#define MOTOR_ADC_TO_mA(adc_value)         (int16_t)map(adc_value, 0, 1023, -5000, 5000)
 
 // Select motor I/O pins so that we can use hw PWM outputs.
 #define MOTOR_DIR_VAL(thrust, forward_val) (thrust > 0 ? forward_val : !forward_val)
+#define MOTOR_PWM_VAL(thrust, dir_val)     (dir_val ? map(abs(thrust), 0, 1023, 255, 0) : map(abs(thrust), 0, 1023, 0, 255))
 
 #define PIN_MOTOR_ENABLE 2
 
@@ -39,3 +43,5 @@
 #define PIN_MOTOR4_PWM 10 // IO4A
 #define PIN_MOTOR4_DIR 11 // IO4B
 #define MOTOR4_DIR_VAL(thrust) MOTOR_DIR_VAL(thrust, false) // DIR is B-pin, so set it low for positive motor voltage
+
+#endif /* _EDUROV_CONFIG_H_ */
--- arduino/eduROV/eduROV.ino
+++ arduino/eduROV/eduROV.ino
@@ -6,6 +6,18 @@ String make_sensor_string(int count, ...) {
 
     va_start(ap, count); /* Requires the last fixed parameter (to get the address) */
     for (int i = 0; i < count; i++) {
+        out_string += String(va_arg(ap, int16_t)) + ","; /* Increments ap to the next argument. */
+    }
+    va_end(ap);
+
+    return out_string.substring(0, out_string.length() - 1);
+}
+String make_actuator_string(int count, ...) {
+    va_list ap;
+    String out_string = "Got actuators: ";
+
+    va_start(ap, count); /* Requires the last fixed parameter (to get the address) */
+    for (int i = 0; i < count; i++) {
         out_string += String(va_arg(ap, int)) + ","; /* Increments ap to the next argument. */
     }
     va_end(ap);
@@ -15,21 +27,35 @@ String make_sensor_string(int count, ...) {
 
 void set_motor_thrust(int dir_pin, int pwm_pin, bool dir_val, int thrust)
 {
-  digitalWrite(dir_pin, dir_val);
   if (thrust == 0)
   {
+    digitalWrite(dir_pin, 0);
     digitalWrite(pwm_pin, 0);
   }
   else
   {
-    analogWrite(pwm_pin, map(abs(thrust), 0, 1000, 0, 256));
+    digitalWrite(dir_pin, dir_val);
+    analogWrite(pwm_pin, MOTOR_PWM_VAL(thrust, dir_val));
   }
 }
 
 void setup() {
   // put your setup code here, to run once:
   
+  pinMode(PIN_LED_PWM, OUTPUT);
+  pinMode(PIN_MOTOR_ENABLE, OUTPUT);
+  pinMode(PIN_MOTOR1_PWM, OUTPUT);
+  pinMode(PIN_MOTOR1_DIR, OUTPUT);
+  pinMode(PIN_MOTOR2_PWM, OUTPUT);
+  pinMode(PIN_MOTOR2_DIR, OUTPUT);
+  pinMode(PIN_MOTOR3_PWM, OUTPUT);
+  pinMode(PIN_MOTOR3_DIR, OUTPUT);
+  pinMode(PIN_MOTOR4_PWM, OUTPUT);
+  pinMode(PIN_MOTOR4_DIR, OUTPUT);
+  
   digitalWrite(PIN_LED_PWM, 0);
+  digitalWrite(PIN_LED_PWM, 0);
+  digitalWrite(PIN_MOTOR_ENABLE, 0);
   digitalWrite(PIN_MOTOR1_PWM, 0);
   digitalWrite(PIN_MOTOR1_DIR, 0);
   digitalWrite(PIN_MOTOR2_PWM, 0);
@@ -39,31 +65,41 @@ void setup() {
   digitalWrite(PIN_MOTOR4_PWM, 0);
   digitalWrite(PIN_MOTOR4_DIR, 0);
   
+  Serial1.begin(115200);
   Serial.begin(115200);
 
-  Serial.println("EduROV Arduino start"); 
+  Serial1.println("EduROV Arduino start"); 
+  Serial.println("EduROV Arduino debug start"); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   static uint32_t messageTime = 0;
   static uint32_t sensor_interval = 500; //how long between each sensor update - milliseconds
+  Serial1.setTimeout(sensor_interval - 10);
+
+  int16_t starboard_thrust = 0;
+  int16_t port_thrust = 0;
+  int16_t vertical_thrust = 0;
+  int16_t led_power = 0;
+  uint32_t new_sensor_interval = 0;
+  bool update_actuators = false;
   
   if ((millis() - messageTime) > sensor_interval) {
     messageTime = millis();
     
     // All ADC measurements are multiplied by 100 to report 2-decimal precision numbers where possible
     
-    int32_t motor1_current = MOTOR_ADC_TO_AMP(analogRead(PIN_SENSE_MOTOR1)) * 100;
-    int32_t motor2_current = MOTOR_ADC_TO_AMP(analogRead(PIN_SENSE_MOTOR2)) * 100;
-    int32_t motor3_current = MOTOR_ADC_TO_AMP(analogRead(PIN_SENSE_MOTOR3)) * 100;
-    int32_t motor4_current = MOTOR_ADC_TO_AMP(analogRead(PIN_SENSE_MOTOR4)) * 100;
+    int16_t motor1_current = MOTOR_ADC_TO_mA(analogRead(PIN_SENSE_MOTOR1));
+    int16_t motor2_current = MOTOR_ADC_TO_mA(analogRead(PIN_SENSE_MOTOR2));
+    int16_t motor3_current = MOTOR_ADC_TO_mA(analogRead(PIN_SENSE_MOTOR3));
+    int16_t motor4_current = MOTOR_ADC_TO_mA(analogRead(PIN_SENSE_MOTOR4));
     
-    int32_t battery =     BATTERY_ADC_TO_VOLT    (analogRead(PIN_SENSE_BATTERY)) * 100;
-    int32_t pressure =    PRESSURE_ADC_TO_KPA    (analogRead(PIN_SENSE_PRESSURE)) * 100;
-    int32_t temperature = TEMPERATURE_ADC_TO_DEGC(analogRead(PIN_SENSE_TEMPERATURE)) * 100;
-  
-    Serial.println(
+    int16_t battery =     BATTERY_ADC_TO_VOLT    (analogRead(PIN_SENSE_BATTERY));
+    int16_t pressure =    PRESSURE_ADC_TO_KPA    (analogRead(PIN_SENSE_PRESSURE));
+    int16_t temperature = TEMPERATURE_ADC_TO_DEGC(analogRead(PIN_SENSE_TEMPERATURE));
+
+    auto sensors = 
       make_sensor_string(7, 
         battery, 
         pressure, 
@@ -72,69 +108,76 @@ void loop() {
         motor2_current, 
         motor3_current, 
         motor4_current
-      )
-    );
+      );
+  
+    Serial.println(sensors);
+    Serial1.println(sensors);
   }
 
-  String indata = Serial.readStringUntil('\n');
+  String indata = Serial1.readStringUntil('\n');
 
-  int32_t starboard_thrust = 0;
-  int32_t port_thrust = 0;
-  int32_t vertical_thrust = 0;
-  int32_t led_power = 0;
-  uint32_t new_sensor_interval = 0;
-
-  int res = sscanf(
-    indata.c_str(), 
-    "interval=%lu", 
-    &new_sensor_interval
-  );
-
-  if (res == 1)
+  if (indata.length() > 1)
   {
-    sensor_interval = new_sensor_interval;
+    Serial.println(indata); 
   }
   
-  res = sscanf(
+  update_actuators |= sscanf(
     indata.c_str(), 
-    "%li, %li, %li, %li", 
+    "star=%i port=%i vert=%i light=%i", 
     &starboard_thrust, 
     &port_thrust, 
     &vertical_thrust, 
     &led_power
-  );
-  if (res != 4)
+  ) == 4;
+  
+  if (update_actuators)
   {
-    // Skip applying power if we didn't get any data.
+    Serial.println(
+      make_actuator_string(4,
+        starboard_thrust, 
+        port_thrust, 
+        vertical_thrust, 
+        led_power
+      )
+    );
+  
+    digitalWrite(PIN_MOTOR_ENABLE, 1);
+  
+    set_motor_thrust(
+      PIN_MOTOR1_DIR, 
+      PIN_MOTOR1_PWM, 
+      MOTOR1_DIR_VAL(starboard_thrust), 
+      starboard_thrust
+    );
+    set_motor_thrust(
+      PIN_MOTOR2_DIR, 
+      PIN_MOTOR2_PWM, 
+      MOTOR2_DIR_VAL(port_thrust), 
+      port_thrust
+    );
+    set_motor_thrust(
+      PIN_MOTOR3_DIR, 
+      PIN_MOTOR3_PWM, 
+      MOTOR3_DIR_VAL(vertical_thrust), 
+      vertical_thrust
+    );
+    set_motor_thrust(
+      PIN_MOTOR4_DIR, 
+      PIN_MOTOR4_PWM, 
+      MOTOR4_DIR_VAL(vertical_thrust), 
+      vertical_thrust
+    );
+  
+    analogWrite(PIN_LED_PWM, led_power); 
     return;
   }
-
-  digitalWrite(PIN_MOTOR_ENABLE, 1);
-
-  set_motor_thrust(
-    PIN_MOTOR1_DIR, 
-    PIN_MOTOR1_PWM, 
-    MOTOR1_DIR_VAL(starboard_thrust), 
-    starboard_thrust
-  );
-  set_motor_thrust(
-    PIN_MOTOR2_DIR, 
-    PIN_MOTOR2_PWM, 
-    MOTOR2_DIR_VAL(port_thrust), 
-    port_thrust
-  );
-  set_motor_thrust(
-    PIN_MOTOR3_DIR, 
-    PIN_MOTOR3_PWM, 
-    MOTOR3_DIR_VAL(vertical_thrust), 
-    vertical_thrust
-  );
-  set_motor_thrust(
-    PIN_MOTOR4_DIR, 
-    PIN_MOTOR4_PWM, 
-    MOTOR4_DIR_VAL(vertical_thrust), 
-    vertical_thrust
-  );
-
-  analogWrite(PIN_LED_PWM, led_power);  
+  
+  bool update_interval = sscanf(indata.c_str(), "interval=%lu", &new_sensor_interval) == 1;
+  if (update_interval)
+  {
+    sensor_interval = new_sensor_interval;
+    Serial1.setTimeout(sensor_interval - 10);
+    Serial.println("Interval = " + sensor_interval);
+    return;
+  }
 }
